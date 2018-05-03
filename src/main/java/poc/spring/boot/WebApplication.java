1773ded1976ca5ee3f05e57af3619c8346f3a9b2
poc-z-cloud <poc.z.cloud@gmail.com>
2018-05-03 16:09:26 -0400

    added react integration

--- src/main/java/poc/spring/boot/WebApplication.java
+++ src/main/java/poc/spring/boot/WebApplication.java
@@ -2,15 +2,23 @@ package poc.spring.boot;
 
 import java.sql.SQLException;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
 import org.h2.tools.Server;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import poc.servlet.JsxConverterServlet;
 
 
 @SpringBootApplication
@@ -25,6 +33,27 @@ public class WebApplication {
         return "redirect:/index.html";
     }
     
+    
+//    @Bean
+//    public ServletContextInitializer contextInitializer() {
+//        return new ServletContextInitializer() {
+//
+//            @Override
+//            public void onStartup(ServletContext servletContext)
+//                    throws ServletException {
+//                    servletContext.setInitParameter("babelJsUri","/js/vendor/babel.min.js");
+//            }
+//        };
+//    }    
+//    
+    // Register Servlet
+    @Bean
+    public ServletRegistrationBean servletRegistrationBean() {
+      ServletRegistrationBean bean = new ServletRegistrationBean(
+          new JsxConverterServlet(), "*.jsx");
+      return bean;
+    }
+
     public static void main(String[] args) throws Exception {
     	String h2BaseDir=null;
     	for (int i=0;i<args.length;i++){
